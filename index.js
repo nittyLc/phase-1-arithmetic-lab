7c65c3fe767ad0565aa08ce046590066720dd58b
caleb <musyokacaleb5@gmail.com>
2024-12-03 17:46:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+
+const max = Math.max(1, 5, 10, 15, 20); // Finds the maximum number in the set
 // Write your code here
